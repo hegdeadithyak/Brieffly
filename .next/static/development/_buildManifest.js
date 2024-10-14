c9757bcb3766e81fe20ba0f7a2163a5244793a8e
hegdeadithyak <hegdeadithyak@gmail.com>
2024-10-14 10:55:27 +0530

    Fixed build issues

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/card":["static\u002Fchunks\u002Fpages\u002Fcard.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcard"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
